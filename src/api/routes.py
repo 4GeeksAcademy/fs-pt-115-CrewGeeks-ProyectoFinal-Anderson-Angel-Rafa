da4ddacd420b513dd3c397e95874a60975aeafd5
Anderson Bernal <andersonbernal7@gmail.com>
2025-08-27 10:11:45 +0000

    routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,11 +12,59 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/companies", methods=["GET"] )
+def get_companies():
+    companies = db.session.query(Company).all()                     #consulta de todas las filas de "company" + all para traer registros
+    return jsonify([c.serialize() for c in companies]), 200         #diccionario JSON // 200 OK
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+
+@api.route("/companies/<int:id>, methods=["GET"])
+def get_company(id):                                                #id para representar la empresa
+    company = db.session.get(company, id)                               #consulta empresa usando id
+    if not company:
+    return jsonify({"error": "company not found"}), 404             #si no existe error 404 y jsonifica el erro
+    return jsonify(company.serialize()), 200                        #respuesta, formato json y metodo serialize. 
+    
+
+
+@api.route("/companies", methods=["POST"])
+def create_company():
+    data = request.jsonify                                          #recibe datos en formato json
+    new_company = company(name=data["name"], cif=data["cif"])       #crear nueva instancia, ejemplo: creweeks, cif 123456789
+    db.session.add(new_company)                                     #agregar nuevo objeto
+    db.session.commit()                                             #ejecuto
+    return jsonify(new_company.serialize()), 201                    #respuesta, formato json y metodo serialize
+
+
+@api.route("/companies/<int:id>", methods="PUT")                    
+def update_company(id):
+    company = db.session.get(company, id)                           #busca la tabla copmany con su id
+    if not company:
+    return jsonify({"error": "company not found"}), 404             #devuelve error
+
+    data = request.json                                             #recibe los nuevos valores en formato JSON
+    company.name = data.get("name", company.name)                   #actualiza el nombre
+    copmany.cif = data.get("cif", company.cif)                      #actualiza el cif
+    db.session.commit()                                             #ejecuto
+    return jsonify(company.serialize()), 200                        #respuesta, formato json y metodo serialize
+
+
+@api.route("/companies/<int:id>", methods=["DELETE"])                #busca la tabla copmany con su id
+def delete_company(id):
+    company = db.session.get(company, id)
+    if not company:                                                 #error por si la empresa no esta registrada
+    return jsonify({"error": "company not found"}), 404
+    db.session.delete(company)                                      #eliminar objeto
+    db.session.commit()                                             #ejecutrar
+    return jsonify(company.serialize()), 200                        #respuesta, formato json y metodo serialize
+
+
+##@api.route('/hello', methods=['POST', 'GET'])
+##def handle_hello():
+##
+##    response_body = {
+##        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+##   }
+##
+##    return jsonify(response_body), 200
