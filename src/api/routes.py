cf8b1acabda83b782f71d4e9a85ac36858438279
Anderson Bernal <andersonbernal7@gmail.com>
2025-08-30 11:41:49 +0000

    feat:imports

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,17 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, Company, Employee, Role
+from api.models import db, Company, Employee, Role, Salary, Payroll, Shifts, Holidays, Suggestions
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+
+
+
+
+
+
+
+
 
 api = Blueprint('api', __name__)
 
