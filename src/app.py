6b05b6eccfe97cf21242d5af028675d83853bc5d
Rafa-progz <rafa.trabajo@outlook.es>
2025-09-06 14:07:21 +0000

    feat: flak email con angel y funcionando

--- src/app.py
+++ src/app.py
@@ -11,6 +11,8 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
+from flask import Flask
+from api.mail_config import mail
 
 # from models import Person
 
@@ -68,6 +70,16 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+#configuracion flask email
+app.config['MAIL_SERVER'] = os.getenv ('MAIL_SERVER')
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.getenv ('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv ('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv ('MAIL_DEFAULT_SENDER')
+mail.init_app (app)
 
 
 # this only runs if `$ python src/main.py` is executed
