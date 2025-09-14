d7632a1d523a27d84d6a5be847073cc89ed010ce
AngelSastre <angel-sc90@hotmail.com>
2025-09-14 12:15:11 +0000

    añadido logo, eliminado slider

--- src/front/components/Navbar/Navbar.jsx
+++ src/front/components/Navbar/Navbar.jsx
@@ -28,15 +28,15 @@ export const Navbar = () => {
 		<header className="header-site">
 			<div className="cg-container cg-nav">
 				<div className="cg-brand">
-					<div className="cg-brand-logo" aria-hidden="true"></div>
+					<img className="cg-logo" src="src/front/assets/img/Logotipo.png" alt="logotipo" />
 					<div className="cg-brand-name">CrewGeeks</div>
 				</div>
+				<div className="cg-cluster">
 				<nav className="cg-nav-links" aria-label="Primary">
 					<a href="#features" className="cg-nav-link">Features</a>
 					<a href="#euteams" className="cg-nav-link">Por qué nosotros</a>
 					<a href="#contact" className="cg-nav-link">Contacto</a>
 				</nav>
-				<div className="cg-cluster">
 					<a href="#trial" className="cg-btn cg-btn--primary">Login</a>
 				</div>
 			</div>
